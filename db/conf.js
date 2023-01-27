b32f847e1df9b06cd1e6be050b67efccedace40c
iAdityarajpurohit <108542528+iAdityarajpurohit@users.noreply.github.com>
2023-01-27 16:01:42 +0530

    commit

---
+++ db/conf.js
@@ -0,0 +1,8 @@
+// const mongoose = require('mongoose');
+
+// mongoose.set('strictQuery', false);
+// mongoose.connect("mongodb://0.0.0.0:27017/e-com")
+
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://aditya:Goty%40007@cluster0.xontiur.mongodb.net/e-com")
